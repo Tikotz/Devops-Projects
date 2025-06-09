c33b6230396639b776f5d147422747dbda311dfc
Tikotz <oritikozki@gmail.com>
2025-06-09 18:36:04 +0300

    deoploy infra

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "us-east-1"
+}
+variable "public_key" {
+    description = "The public key for SSH access to the instance"
+    type        = string
+}
+variable "private_key" {
+    description = "The private key for SSH access to the instance"
+    type        = string
+}
+variable "key_name" {
+    description = "The name of the SSH key pair to use for the instance"
+    type        = string
+}
